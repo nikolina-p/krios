e63ee7cf3b4a3de035ee86b50225e581dfde57ec
nikolina <nikolinap85@gmail.com>
2019-04-08 18:56:28 +0200

    Data fixtures added.

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Patient;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
@@ -9,8 +10,14 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $patient = new Patient();
+        $patient->setName('Mika');
+        $patient->setSurname('Stefanovic');
+        $patient->setEmail('mika#gmail.com');
+        $patient->setGender('MALE');
+        $patient->setPhone('+38167123456');
+
+        $manager->persist($patient);
 
         $manager->flush();
     }
--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -18,8 +18,6 @@ class UserFixtures extends Fixture
 
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
         $user = new User();
         $user->setName('Nemanja');
         $user->setSurname("Mostarac");
