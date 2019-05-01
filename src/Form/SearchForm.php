e1b6494385569c83b38fd6d3e6a1b314ea994f22
nikolina <nikolinap85@gmail.com>
2019-05-01 16:36:52 +0200

    Setting Search field to be 'not required'.

--- src/Form/SearchForm.php
+++ src/Form/SearchForm.php
@@ -1,11 +1,10 @@
 <?php
 
-
 namespace App\Form;
 
 use App\DTO\PatientDTO;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SearchType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,8 +12,10 @@ class SearchForm extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('searchTerm', TextType::class, [
-            'label' => false
+        $builder->add('searchTerm', SearchType::class, [
+            'label' => false,
+            'required' => false,
+            'empty_data' => ""
         ]);
     }
 
